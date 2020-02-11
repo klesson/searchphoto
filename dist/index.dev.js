16dc23d1c6604456fb7878e638a5a86f96c10d91
klesson <49154904+klesson@users.noreply.github.com>
2020-02-11 19:26:28 +0300

    compiled files
    need for lesson

---
+++ dist/index.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var Unsplash = require('unsplash-js')["default"];
+
+var unsplash = new Unsplash({
+  accessKey: "1351f42e461314bb24eadc303fce74348ff776d91a08389d45f0dd54853f766a"
+});
+console.log('un', unsplash);
